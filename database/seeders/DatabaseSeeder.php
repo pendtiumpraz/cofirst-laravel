e8c442bd004e4d144cceb42bd56c7e7141ff7955
pendtiumpraz <pendtiumpraz@gmail.com>
2025-07-05 18:20:28 +0700

    feat: add GamificationSeeder to DatabaseSeeder
    - Add gamification seeder to run after all other seeders
    - Seeds initial badges and rewards for the gamification system

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,5 +38,8 @@ class DatabaseSeeder extends Seeder
         $this->call(ReportSeeder::class);
         $this->call(FinancialTransactionSeeder::class);
         $this->call(AnnouncementSeeder::class);
+        
+        // 9. Seed gamification data (badges and rewards).
+        $this->call(GamificationSeeder::class);
     }
 }
